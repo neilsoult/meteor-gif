7835e4d868d68e122ab0c4b2d754b6a671704306
Neil Soult <neil.soult@gmail.com>
2017-10-19 17:54:10 -0700

    first prototype

--- client/home.js
+++ client/home.js
@@ -1,7 +1,59 @@
+import gifshot from 'gifshot';
+let resetPage = () => {
+    $('.container').removeClass('dn');
+    $('.result').addClass('dn');
+    $('form')[0].reset();
+};
+
+Template.body.onRendered(() => {
+    resetPage();
+    let $dropzone = $('#dropDiv');
+    $dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    })
+    .on('dragover dragenter', () => {
+        $dropzone.addClass('dragging');
+    })
+    .on('dragleave dragend drop', () => {
+        $dropzone.removeClass('dragging');
+    })
+    .on('drop', (e) => {
+        console.log('drop event', e);
+        _.each(e.originalEvent.dataTransfer.files, (file) => {
+            console.log('input', file);
+            Meteor.saveFile(file, file.name, null, file.type, () => {});
+        });
+    });
+    $('form').on('submit', (e) => {
+        e.preventDefault();
+        let milliseconds = e.target.ms.value;
+        let files = Meteor.getFiles();
+        let fileNodes = Object.keys(Meteor.getFiles()).map((node) => {
+            return files[node];
+        });
+
+        if (milliseconds > 0 && fileNodes.length > 1) {
+            gifshot.createGIF({
+                'images': fileNodes,
+                'frameDuration': milliseconds
+            }, (obj) => {
+                $('.result img').attr('src', obj.image);
+                $('.result a').attr('href', obj.image);
+                $('.container').addClass('dn');
+                $('.result').removeClass('dn');
+            });
+        }
+    });
+    $('button.reset').on('click', resetPage);
+});
+
 Template.gifmaker.events({
-    'change input': function(ev) {
-        _.each(ev.target.files, function(file) {
-            Meteor.saveFile(file, file.name);
+    'change input': (event) => {
+        console.log('index', $('form input').index($(event.target)));
+        _.each(event.target.files, (file) => {
+            console.log('input', file);
+            Meteor.saveFile(file, file.name, null, file.type, () => {});
         });
     }
 });
--- packages/meteor-gifmaker/client/save_file.js
+++ packages/meteor-gifmaker/client/save_file.js
@@ -1,20 +1,31 @@
+let fileNodes = {};
+
+Meteor.getFiles = () => {
+    return fileNodes;
+};
+
 Meteor.saveFile = function(blob, name, path, type, callback) {
     var fileReader = new FileReader(),
-        method, encoding = 'binary', type = type || 'binary';
+        method, encoding = 'binary';
+    type = type || 'binary';
     switch (type) {
         case 'image/png':
         case 'image/jpeg':
         case 'binary':
-            method = 'readAsBinaryString';
+            method = 'readAsDataURL';
             encoding = 'binary';
             break;
         default:
-            method = 'readAsBinaryString';
+            method = 'readAsDataURL';
             encoding = 'binary';
             break;
     }
     fileReader.onload = function(file) {
        console.log(file);
+       let img = document.createElement('img');
+       img.src = file.target.result;
+       fileNodes[name] = img;
+       callback(file);
     }
     fileReader[method](blob);
 }
